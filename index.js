87b7a4f707ede4207b54555acf8aca56a9c291ca
uhikarudevjapan <walrus4l145@icloud.com>
2022-06-29 16:38:33 +0900

    npm init

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-async function main() {
-    try {
-        const userId = getUserId();
-        const userInfo = await fetchUserInfo(userId);
-        const view = createView(userInfo);
-        displayView(view);
-    } catch (error) {
-        console.error(`エラーが発生しました (${error})`);
-    }
+async function main () {
+  try {
+    const userId = getUserId()
+    const userInfo = await fetchUserInfo(userId)
+    const view = createView(userInfo)
+    displayView(view)
+  } catch (error) {
+    console.error(`エラーが発生しました (${error})`)
+  }
 }
 
-function fetchUserInfo(userId) {
-    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
-            } else {
-                return response.json();
-            }
-        });
+function fetchUserInfo (userId) {
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`))
+      } else {
+        return response.json()
+      }
+    })
 }
 
-function getUserId() {
-    return document.getElementById("userId").value;
+function getUserId () {
+  return document.getElementById('userId').value
 }
 
-function createView(userInfo) {
-    return escapeHTML`
+function createView (userInfo) {
+  return escapeHTML`
     <h4>${userInfo.name} (@${userInfo.login})</h4>
     <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
     <dl>
@@ -34,31 +34,30 @@ function createView(userInfo) {
         <dt>Repositories</dt>
         <dd>${userInfo.public_repos}</dd>
     </dl>
-    `;
+    `
 }
 
-function displayView(view) {
-    const result = document.getElementById("result");
-    result.innerHTML = view;
+function displayView (view) {
+  const result = document.getElementById('result')
+  result.innerHTML = view
 }
 
-function escapeSpecialChars(str) {
-    return str
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+function escapeSpecialChars (str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
 }
 
-function escapeHTML(strings, ...values) {
-    return strings.reduce((result, str, i) => {
-        const value = values[i - 1];
-        if (typeof value === "string") {
-            return result + escapeSpecialChars(value) + str;
-        } else {
-            return result + String(value) + str;
-        }
-    });
+function escapeHTML (strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1]
+    if (typeof value === 'string') {
+      return result + escapeSpecialChars(value) + str
+    } else {
+      return result + String(value) + str
+    }
+  })
 }
-
